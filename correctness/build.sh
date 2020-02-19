7e63f9dcbbbb9b338069e281ea101f2cd42d133a
Jin Gan <jamiegan835@gmail.com>
2020-02-19 12:12:50 -0500

    correctness/build.sh: 1. remove unnecessary set up command. 2. "ldconfig" is for updating c++ shared library's cache, just run every time before comply to make sure using the right library. 3. update g++ command.

--- correctness/build.sh
+++ correctness/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-pushd ~/jaeger-client-cpp/build
-make install
-popd
-
-g++ -O3 -march=native -flto -std=c++11 -lopentracing -ljaegertracing -lpthread ~/jaeger-client-cpp/build/libjaegertracing.a main.cpp
+#pushd ~/jaeger-client-cpp/build
+#make install
+#popd
+sudo ldconfig
+#g++ -O3 -march=native -flto -std=c++11 -lopentracing -ljaegertracing -lpthread ~/jaeger-client-cpp/build/libjaegertracing.a main.cpp
+g++ -O3 -march=native -flto -std=c++11 ~/skua-jaeger-client-cpp/build/libjaegertracing.a main.cpp -L/usr/local/lib/ -lopentracing -ljaegertracing -lpthread
