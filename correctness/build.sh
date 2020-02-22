f2108c9ec431697ff7973ca5f579ef9446c7ce22
Jin Gan <jamiegan835@gmail.com>
2020-02-22 15:04:40 -0500

    Update the build script for correctness test program.

--- correctness/build.sh
+++ correctness/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#pushd ~/jaeger-client-cpp/build
-#make install
-#popd
-sudo ldconfig
-#g++ -O3 -march=native -flto -std=c++11 -lopentracing -ljaegertracing -lpthread ~/jaeger-client-cpp/build/libjaegertracing.a main.cpp
+sudo ldconfig # rebuild shared library cache
+
 g++ -O3 -march=native -flto -std=c++11 ~/skua-jaeger-client-cpp/build/libjaegertracing.a main.cpp -L/usr/local/lib/ -lopentracing -ljaegertracing -lpthread
